08f8497ce0e233186254334534982e3580c745e6
Alexander Chernyh <alexchernyh2000@mail.ru>
2023-11-09 17:04:37 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, soluta? Aperiam, atque sapiente fugiat delectus, quam assumenda et placeat accusantium consectetur illum in ducimus error incidunt, sit velit officia voluptatum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro quidem nam at ipsa, tempora adipisci explicabo quaerat sapiente praesentium esse, eveniet quisquam voluptatem expedita! Veritatis aliquid vel rerum dolorum perferendis!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p {
+    margin: 20px 0;
+}
+</style>
